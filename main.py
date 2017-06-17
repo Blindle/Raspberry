0de033023812b17992db28e078188a5e20f1ce45
Emanuel <emanuel.suriano@gmail.com>
2017-06-17 14:46:56 -0300

    added arduino communication test

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from Level import Level
 import musicPlayer
+import serial
 
 mode = 'evaluation'
 level = Level(1, mode)
 
 level.print_words_in_level()
 
-musicPlayer.play("audio.mp3")
+#musicPlayer.play("audio.mp3")
+
+
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+print("Starting!")
+
+while True:
+      comando = raw_input('Introduce un comando: ') #Input
+      arduino.write(comando) #Mandar un comando hacia Arduino
+      if comando == 'H':
+            print('LED ENCENDIDO')
+      elif comando == 'L':
+            print('LED APAGADO')
+
+arduino.close() #Finalizamos la comunicacion
