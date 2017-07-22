9722e2e68658266ad1fe3511c16317a17fb4ef4e
Joaquin Fontana <jfontana94@gmail.com>
2017-07-22 10:49:26 -0300

    First iteration of learn module

--- src/state/objects/Learn.py
+++ src/state/objects/Learn.py
@@ -5,14 +5,34 @@ sys.path.append(os.path.dirname(__file__) + "/../../")
 from state import state
 from state.stateEnum import StateEnum
 from helpers import configHelper
+from helpers import loadingHelper
 
 class Learn:
+
+    current_word = 0
+
     def __init__(self, level_number):
         self.number = level_number
         self.words = configHelper.get_level_config('learn', self.number)['words']
+        self.output = loadingHelper.get_output_processer();
         print("Nivel " + str(self.number) + " de aprendizaje")
+        print(self.words[self.current_word])
 
     def process_input(self, input_value):
+
+        if input_value == "right":
+            self.current_word += 1
+            self._verify_overflow()
+            word = self.words[self.current_word]
+            print(word)
+            self.output.write(word.upper())
+
+        if input_value == "left":
+            self.current_word -= 1
+            self._verify_overflow()
+            word = self.words[self.current_word]
+            print(word)
+            self.output.write(word.upper())
 
         if input_value == "back":
             print("Regresando a " + StateEnum.MENU.realName)
